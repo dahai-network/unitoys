d76dfa0f6c2339aeb17db128feabb5ba5ec268f2
214978335@qq.com <214978335@qq.com>
2017-05-02 10:40:17 +0800

    1.增加TAB页面分页功能；2.更换服务页面Banner图片

--- UnitoysWebSite/Content/js/common.js
+++ UnitoysWebSite/Content/js/common.js
@@ -1,16 +1,25 @@
-$(function(){
-	$(".nav .item1").hover(function(){
-		$(".product_second").show();
-		$(this).find("a").addClass("active");
-	},function(){
-		$(".product_second").hide();
-		$(this).find("a").removeClass("active");
-	});
-	$(".product_second").hover(function(){
-		$(this).show();
-		$(".nav .item1 a").addClass("active");
-	},function(){
-		$(this).hide();
-		$(".nav .item1 a").removeClass("active");
-	})
+$(function () {
+    $(".nav .item1").hover(function () {
+        $(".product_second").show();
+        $(this).find("a").addClass("active");
+    }, function () {
+        $(".product_second").hide();
+        $(this).find("a").removeClass("active");
+    });
+    $(".product_second").hover(function () {
+        $(this).show();
+        $(".nav .item1 a").addClass("active");
+    }, function () {
+        $(this).hide();
+        $(".nav .item1 a").removeClass("active");
+    })
+
+    $(".service_page .p_nav>ul>li>a,.about_main .p_nav>ul>li>a").click(function () {
+        //alert($(this).data("tab"));
+        $(".p_nav>ul>li>a").removeClass('cur');
+        $('.tab-content').removeClass('tab-content-show');
+        $(this).addClass('cur');
+        $($(this).data("tab")).addClass('tab-content-show');
+    });
+    
 })
