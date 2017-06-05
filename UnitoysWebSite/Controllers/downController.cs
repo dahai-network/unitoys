4643d99bd79feb2e2222b5d849e9f39f2970bc2c
214978335@qq.com <214978335@qq.com>
2017-06-05 14:46:09 +0800

    1.下载页面修改

---
+++ UnitoysWebSite/Controllers/downController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnitoysWebSite.Controllers
+{
+    public class downController : Controller
+    {
+        //
+        // GET: /Down/
+        public ActionResult Index()
+        {
+            return Redirect("http://a.app.qq.com/o/simple.jsp?pkgname=cn.com.aixiaoqi");//2
+        }
+	}
+}
